287a54006f491db7dab0e31c3a72b3263612a04d
hammertime1070 <mbkeith614@gmail.com>
2023-08-22 11:33:49 -0400

    Complete exercise

--- script.js
+++ script.js
@@ -1,24 +1,11 @@
 function getComputerChoice() {
     var options = ['Rock', 'Paper', 'Scissors'];
-    console.log(options);
     var selection = options[Math.floor(Math.random() * options.length)];
     return selection
 }
 
-function singleRound(playerSelection, computerSelection) {
-    if (playerSelection === computerSelection) {
-        console.log("You tied!")
-    }
-    else if (playerSelection === 'Scissors' && computerSelection === 'Paper') {
-        console.log('You Win! Scissors beats Paper!')
-    }
-    else if (playerSelection === 'Scissors' && computerSelection === 'Rock') {
-        console.log('You lose! Rock beats Scissors!')
-    }
 
-}
-
-function otherSingleRound(playerSelection, computerSelection) {
+function SingleRound(playerSelection, computerSelection) {
     var playerSelection = playerSelection.toUpperCase();
     var computerSelection = computerSelection.toUpperCase();
     var victoryMessage = "You Win! " + playerSelection + " beats " + computerSelection + "!"
@@ -27,24 +14,24 @@ function otherSingleRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
         return tieMessage
     }
-    else if (playerSelection === 'Rock') {
-        if (computerSelection === 'Scissors') {
+    else if (playerSelection === 'ROCK') {
+        if (computerSelection === 'SCISSORS') {
             return victoryMessage
         }
         else {
             return defeatMessage
         }
     }
-    else if (playerSelection === 'Paper') {
-        if (computerSelection === 'Rock') {
+    else if (playerSelection === 'PAPER') {
+        if (computerSelection === 'ROCK') {
             return victoryMessage
         }
         else {
             return defeatMessage
         }
     }
-    else if (playerSelection === 'Scissors') {
-        if (computerSelection === 'Paper') {
+    else if (playerSelection === 'SCISSORS') {
+        if (computerSelection === 'PAPER') {
             return victoryMessage
         }
         else {
@@ -54,3 +41,13 @@ function otherSingleRound(playerSelection, computerSelection) {
 }
 
 
+function game() {
+    var i = 0;
+    while (i < 5) {
+        let playerSelection = prompt("ROCK PAPER SCISSORS SHOOT!");
+        console.log(SingleRound(playerSelection=playerSelection, computerSelection=getComputerChoice()));
+        i++;
+    }
+}
+
+game()
